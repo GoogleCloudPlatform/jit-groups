1498d3eac6421de1db28f9a12b6f035dae8f8e93
Johannes Passing <jpassing@hotmail.com>
2023-12-18 11:09:01 +1100

    Change accessibility

--- sources/src/main/java/com/google/solutions/jitaccess/core/activation/project/ProjectRoleActivator.java
+++ sources/src/main/java/com/google/solutions/jitaccess/core/activation/project/ProjectRoleActivator.java
@@ -6,26 +6,33 @@ import com.google.solutions.jitaccess.core.AccessException;
 import com.google.solutions.jitaccess.core.UserId;
 import com.google.solutions.jitaccess.core.activation.*;
 import com.google.solutions.jitaccess.core.clients.ResourceManagerClient;
+import jakarta.enterprise.context.ApplicationScoped;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Collection;
 
 /**
  * Activator for project roles.
  */
+@ApplicationScoped
 public class ProjectRoleActivator extends EntitlementActivator<ProjectRoleId> {
   private final ResourceManagerClient resourceManagerClient;
+  private final Options options;
 
   public ProjectRoleActivator(
     EntitlementCatalog<ProjectRoleId> catalog,
     ResourceManagerClient resourceManagerClient,
-    JustificationPolicy policy
+    JustificationPolicy policy,
+    Options options
   ) {
     super(catalog, policy);
 
     Preconditions.checkNotNull(resourceManagerClient, "resourceManagerClient");
+    Preconditions.checkNotNull(options, "options");
 
     this.resourceManagerClient = resourceManagerClient;
+    this.options = options;
   }
 
   /**
@@ -43,7 +50,7 @@ public class ProjectRoleActivator extends EntitlementActivator<ProjectRoleId> {
   // Inner classes.
   // -------------------------------------------------------------------------
 
-  public static class JitRequest extends JitActivationRequest<ProjectRoleId> {
+  static class JitRequest extends JitActivationRequest<ProjectRoleId> {
     public JitRequest(
       UserId requestingUser,
       Collection<ProjectRoleId> entitlements,
@@ -55,7 +62,7 @@ public class ProjectRoleActivator extends EntitlementActivator<ProjectRoleId> {
     }
   }
 
-  public static class MpaRequest extends MpaActivationRequest<ProjectRoleId> {
+  static class MpaRequest extends MpaActivationRequest<ProjectRoleId> {
     public MpaRequest(
       UserId requestingUser,
       Collection<ProjectRoleId> entitlements,
@@ -69,6 +76,30 @@ public class ProjectRoleActivator extends EntitlementActivator<ProjectRoleId> {
       if (entitlements.size() != 1) {
         throw new IllegalArgumentException("Only one entitlement can be activated at a time");
       }
+    }
+  }
+
+  // -------------------------------------------------------------------------
+  // Inner classes.
+  // -------------------------------------------------------------------------
+
+  public record Options (
+   Duration maxActivationTimeout,
+   int minNumberOfReviewersPerActivationRequest,
+   int maxNumberOfReviewersPerActivationRequest
+  ) {
+    static final int MIN_ACTIVATION_TIMEOUT_MINUTES = 5;
+
+    public Options {
+      Preconditions.checkArgument(
+        maxActivationTimeout.toMinutes() >= MIN_ACTIVATION_TIMEOUT_MINUTES,
+        "Activation timeout must be at least 5 minutes");
+      Preconditions.checkArgument(
+        minNumberOfReviewersPerActivationRequest > 0,
+        "The minimum number of reviewers cannot be 0");
+      Preconditions.checkArgument(
+        minNumberOfReviewersPerActivationRequest <= maxNumberOfReviewersPerActivationRequest,
+        "The minimum number of reviewers must not exceed the maximum");
     }
   }
 }
