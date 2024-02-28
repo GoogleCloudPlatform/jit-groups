e4cc910685b0042ea419974583be1bdd01b240c9
Johannes Passing <jpassing@hotmail.com>
2024-02-28 14:32:12 +1100

    Remove policy changes

--- sources/src/main/java/com/google/solutions/jitaccess/core/auth/GroupExpander.java
+++ sources/src/main/java/com/google/solutions/jitaccess/core/auth/GroupExpander.java
@@ -34,7 +34,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 /**
- * Expands
+ * Expands groups in access control lists.
  */
 public class GroupExpander {
   private final @NotNull CloudIdentityGroupsClient groupsClient;
