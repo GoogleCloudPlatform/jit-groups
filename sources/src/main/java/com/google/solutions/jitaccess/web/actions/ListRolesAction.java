ba2b139c5e3d16446d29b6ab6cf5a5d66063d599
Johannes Passing <jpassing@hotmail.com>
2024-03-06 17:45:51 +1100

    b/325372861 Move Exceptions class to util package (#315)

--- sources/src/main/java/com/google/solutions/jitaccess/web/actions/ListRolesAction.java
+++ sources/src/main/java/com/google/solutions/jitaccess/web/actions/ListRolesAction.java
@@ -24,7 +24,7 @@ package com.google.solutions.jitaccess.web.actions;
 import com.google.common.base.Preconditions;
 import com.google.solutions.jitaccess.core.AccessDeniedException;
 import com.google.solutions.jitaccess.core.AccessException;
-import com.google.solutions.jitaccess.core.Exceptions;
+import com.google.solutions.jitaccess.core.util.Exceptions;
 import com.google.solutions.jitaccess.core.RoleBinding;
 import com.google.solutions.jitaccess.core.catalog.ActivationType;
 import com.google.solutions.jitaccess.core.catalog.Entitlement;
