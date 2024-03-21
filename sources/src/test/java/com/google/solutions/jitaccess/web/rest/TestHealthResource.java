8403b79594aae15db074e55c43fce6e17ef8829b
Johannes Passing <jpassing@hotmail.com>
2024-03-21 11:07:04 +1100

    b/330263705 Fix post-rebase conflicts

--- sources/src/test/java/com/google/solutions/jitaccess/web/rest/TestHealthResource.java
+++ sources/src/test/java/com/google/solutions/jitaccess/web/rest/TestHealthResource.java
@@ -21,7 +21,7 @@
 
 package com.google.solutions.jitaccess.web.rest;
 
-import com.google.solutions.jitaccess.core.auth.UserEmail;
+import com.google.solutions.jitaccess.core.auth.UserId;
 import com.google.solutions.jitaccess.core.clients.DiagnosticsResult;
 import com.google.solutions.jitaccess.web.LogAdapter;
 import com.google.solutions.jitaccess.web.RestDispatcher;
@@ -36,7 +36,7 @@ import java.util.concurrent.Executor;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestHealthResource {
-  private static final UserEmail SAMPLE_USER = new UserEmail("user-1@example.com");
+  private static final UserId SAMPLE_USER = new UserId("user-1@example.com");
 
   //---------------------------------------------------------------------------
   // /health/alive
