GoogleCloudPlatform/jit-access

b/325372861 Group-based catalog (WIP)

sources/src/main/java/com/google/solutions/jitaccess/core/auth/AccessControlList.java
@@ -21,23 +21,171 @@
 
 package com.google.solutions.jitaccess.core.auth;
 
+import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Allows access to a defined set of principals.
- * @param allowedPrincipals allowed principals
+ *
+ * @param entries list of access control list entries.
  */
 public record AccessControlList(
-  @NotNull Set<PrincipalIdentifier> allowedPrincipals
+  @NotNull Collection<Entry> entries
 ){
   /**
-   * Check whether a subject is allowed access.
+   * Check whether access is allowed access for any of the provided
+   * principals.
+   *
+   * Access control list entries are evaluated in order. Access is
+   * denied as soon as a matching deny-entry is encountered.
    */
-  public boolean isAllowed(@NotNull Subject subject) {
-    return this.allowedPrincipals
+  private static boolean isAllowed(
+    @NotNull Iterable<PrincipalIdentifier> principals,
+    Iterable<Entry> accessEntries,
+    int requiredAccessRights
+  ) {
+    Preconditions.checkArgument(requiredAccessRights != 0, "requiredAccessMask");
+
+    //
+    // NB. It's possible that no single principal is granted all the requested
+    // access. Therefore, we can't check each principal individually, but
+    // instead have to calculate the effective access mask across all principals.
+    //
+
+    int effectiveAccessMaskForSubject = 0;
+
+    for (var principal : principals) {
+      for (var entry : accessEntries) {
+        if (entry.principal.equals(principal)) {
+          if (entry instanceof AllowedEntry allowedEntry) {
+            //
+            // Accumulate access granted by this entry.
+            //
+            effectiveAccessMaskForSubject |= allowedEntry.accessRights;
+          }
+          else if (entry instanceof DeniedEntry deniedEntry &&
+            (deniedEntry.accessRights & requiredAccessRights) != 0) {
+            //
+            // At least one of the required access bits is denied.
+            //
+            return false;
+          }
+        }
+      }
+    }
+
+    return (effectiveAccessMaskForSubject & requiredAccessRights) == requiredAccessRights;
+  }
+
+  /**
+   * @return principals that have granted the requested level of access.
+   */
+  public Set<PrincipalIdentifier> allowedPrincipals(int requiredAccessRights) {
+    //
+    // Segment the list of entries by principal.
+    //
+    var entriesByPrincipals = this.entries
       .stream()
-      .anyMatch(id -> subject.principals().contains(id));
+      .collect(Collectors.groupingBy(e -> e.principal));
+
+    //
+    // Filter down the list of principals to those that have been
+    // granted the requested level of access.
+    //
+    return entriesByPrincipals
+      .entrySet()
+      .stream()
+      .filter(e -> isAllowed(
+        Collections.singleton(e.getKey()), // Principal
+        e.getValue(), // List of access control list entries
+        requiredAccessRights))
+      .map(e -> e.getKey())
+      .collect(Collectors.toSet());
+  }
+
+  /**
+   * Check whether a subject is allowed access.
+   *
+   * Access control list entries are evaluated in order. Access is
+   * denied as soon as a matching deny-entry is encountered.
+   */
+  public boolean isAllowed(@NotNull Subject subject, int requiredAccessRights) {
+    Preconditions.checkArgument(requiredAccessRights != 0, "requiredAccessRights");
+
+    return isAllowed(subject.principals(), this.entries, requiredAccessRights);
+  }
+
+  //---------------------------------------------------------------------------
+  // Inner classes.
+  //---------------------------------------------------------------------------
+
+  /**
+   * An access control list entry.
+   */
+  public static abstract class Entry {
+    /**
+     * Kind of access granted, where each bit represents a
+     * particular kind of access.
+     */
+    public final int accessRights;
+
+    /**
+     * Principal that is being granted access.
+     */
+    public final @NotNull PrincipalIdentifier principal;
+
+    protected Entry(@NotNull PrincipalIdentifier principal, int accessRights) {
+      this.principal = principal;
+      this.accessRights = accessRights;
+    }
+  }
+
+  /**
+   * Entry that grants certain access.
+   */
+  public static class AllowedEntry extends Entry {
+    public AllowedEntry(@NotNull PrincipalIdentifier principal, int accessMask) {
+      super(principal, accessMask);
+    }
+
+    @Override
+    public String toString() {
+      return String.format("Allow %s: %08X", this.principal, this.accessRights);
+    }
+  }
+
+  /**
+   * Entry that denies certain access.
+   */
+  public static class DeniedEntry extends Entry {
+    public DeniedEntry(@NotNull PrincipalIdentifier principal, int accessMask) {
+      super(principal, accessMask);
+    }
+
+    @Override
+    public String toString() {
+      return String.format("Deny %s: %08X", this.principal, this.accessRights);
+    }
+  }
+
+  public static class Builder {
+    private final List<Entry> entries = new LinkedList<>();
+
+    public Builder allow(PrincipalIdentifier principal, int accessMask) {
+      this.entries.add(new AllowedEntry(principal, accessMask));
+      return this;
+    }
+
+    public Builder deny(PrincipalIdentifier principal, int accessMask) {
+      this.entries.add(new DeniedEntry(principal, accessMask));
+      return this;
+    }
+
+    public AccessControlList build() {
+      return new AccessControlList(Collections.unmodifiableList(this.entries));
+    }
   }
 }

sources/src/main/java/com/google/solutions/jitaccess/core/catalog/jitrole/JitRoleActivator.java
@@ -1,0 +1,140 @@
+//
+// Copyright 2024 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+package com.google.solutions.jitaccess.core.catalog.jitrole;
+
+import com.google.common.base.Preconditions;
+import com.google.solutions.jitaccess.core.*;
+import com.google.solutions.jitaccess.core.auth.UserEmail;
+import com.google.solutions.jitaccess.core.catalog.*;
+import com.google.solutions.jitaccess.core.clients.CloudIdentityGroupsClient;
+import org.checkerframework.checker.units.qual.N;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.InstantSource;
+
+/**
+ * Activates JIT roles by modifying memberships to the Google group that backs
+ * the JIT role.
+ */
+public class JitRoleActivator extends EntitlementActivator<JitRole, OrganizationId, UserContext> {
+  private final @NotNull CloudIdentityGroupsClient groupsClient;
+  private final @NotNull JitRoleGroupMapping mapping;
+  private final @NotNull InstantSource clock;
+
+  public JitRoleActivator(
+    @NotNull Catalog<JitRole, OrganizationId, UserContext> catalog,
+    @NotNull CloudIdentityGroupsClient groupsClient,
+    @NotNull JitRoleGroupMapping mapping,
+    @NotNull JustificationPolicy policy,
+    @NotNull InstantSource clock
+  ) {
+    super(catalog, policy);
+
+    Preconditions.checkNotNull(groupsClient, "groupsClient");
+
+    this.groupsClient = groupsClient;
+    this.mapping = mapping;
+    this.clock = clock;
+  }
+
+  private Activation provisionGroupMembership(
+    @NotNull JitRole role,
+    @NotNull UserEmail user,
+    @NotNull Instant startTime,
+    @NotNull Duration duration
+  ) throws AccessException, IOException {
+
+    var group = this.mapping.groupFromJitRole(role);
+
+    //
+    // Add time-bound group membership. If the user is a member
+    // already, update the expiry of their membership.
+    //
+    // NB. Unlike with IAM conditions, we can't specify a start time
+    // for a temporary group memberships.
+    //
+    Preconditions.checkArgument(
+      !startTime.isAfter(this.clock.instant()),
+      "Start time must not be in the future");
+
+    this.groupsClient.addMembership(group, user, startTime.plus(duration));
+
+    return new Activation(startTime, duration);
+  }
+
+  //---------------------------------------------------------------------------
+  // Overrides.
+  //---------------------------------------------------------------------------
+
+  @Override
+  public int maximumNumberOfEntitlementsPerJitRequest() {
+    return 1;
+  }
+
+  @Override
+  protected Activation provisionAccess(
+    JitActivationRequest<JitRole> request
+  ) throws AccessException, AlreadyExistsException, IOException {
+    Preconditions.checkNotNull(request, "request");
+    Preconditions.checkArgument(
+      request.entitlements().size() == 1,
+      "Only one JIT role can be activated at a time");
+
+    return provisionGroupMembership(
+      request.entitlements().stream().findFirst().get(),
+      request.requestingUser(),
+      request.startTime(),
+      request.duration());
+  }
+
+  @Override
+  protected Activation provisionAccess(
+    UserEmail approvingUser,
+    MpaActivationRequest<JitRole> request
+  ) throws AccessException, AlreadyExistsException, IOException {
+    Preconditions.checkNotNull(request, "request");
+    Preconditions.checkArgument(
+      request.entitlements().size() == 1,
+      "Only one JIT role can be activated at a time");
+
+    //
+    // NB. The start/end time for the membership is derived from the approval token. If multiple
+    // reviewers try to approve the same token, the resulting condition (and binding) will
+    // be the same.
+    //
+
+    return provisionGroupMembership(
+      request.entitlements().stream().findFirst().get(),
+      request.requestingUser(),
+      request.startTime(),
+      request.duration());
+  }
+
+  @Override
+  public @NotNull JsonWebTokenConverter<MpaActivationRequest<JitRole>> createTokenConverter() {
+    //TODO: add membership!
+    throw new RuntimeException("NIY!");
+  }
+}

sources/src/test/java/com/google/solutions/jitaccess/core/auth/TestGroupExpander.java
@@ -1,0 +1,110 @@
+//
+// Copyright 2024 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+package com.google.solutions.jitaccess.core.auth;
+
+import com.google.api.services.cloudidentity.v1.model.EntityKey;
+import com.google.api.services.cloudidentity.v1.model.Membership;
+import com.google.solutions.jitaccess.core.clients.CloudIdentityGroupsClient;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Executor;
+
+public class TestGroupExpander {
+  private static final UserEmail TEST_USER_1 = new UserEmail("test-1@example.com");
+  private static final UserEmail TEST_USER_2 = new UserEmail("test-2@example.com");
+  private static final GroupEmail TEST_GROUP_1 = new GroupEmail("group-1@example.com");
+  private static final GroupEmail TEST_GROUP_2 = new GroupEmail("group-2@example.com");
+
+  private class SynchronousExecutor implements Executor {
+
+    @Override
+    public void execute(Runnable command) {
+      command.run();
+    }
+  }
+
+  @Test
+  public void whenSetEmpty_ThenExpandReturnsEquivalentSet() throws Exception {
+    var groupsClient = mock(CloudIdentityGroupsClient.class);
+    var expander = new GroupExpander(groupsClient, new SynchronousExecutor());
+
+    var inputSet = Set.<PrincipalIdentifier>of();
+    var outputSet = expander.expand(inputSet);
+    assertEquals(0, outputSet.size());
+  }
+
+  @Test
+  public void whenSetContainsNoGroups_ThenExpandReturnsEquivalentSet() throws Exception {
+    var groupsClient = mock(CloudIdentityGroupsClient.class);
+    var expander = new GroupExpander(groupsClient, new SynchronousExecutor());
+
+    var inputSet = Set.<PrincipalIdentifier>of(
+      TEST_USER_1,
+      TEST_USER_2);
+    var outputSet = expander.expand(inputSet);
+
+    assertEquals(2, outputSet.size());
+    assertTrue(outputSet.contains(TEST_USER_1));
+    assertTrue(outputSet.contains(TEST_USER_2));
+  }
+
+  @Test
+  public void whenSetContainsGroups_ThenExpandReturnsExpandedSet() throws Exception {
+    var member1 = new UserEmail("member-1@example.com");
+    var member2 = new GroupEmail("member-1@example.com");
+
+    var groupsClient = mock(CloudIdentityGroupsClient.class);
+    when(groupsClient.listMemberships(TEST_GROUP_1))
+      .thenReturn(List.of(new Membership()
+        .setType("SERVICE_ACCOUNT")
+        .setPreferredMemberKey(new EntityKey().setId("sa@example.com"))));
+    when(groupsClient.listMemberships(TEST_GROUP_2))
+      .thenReturn(List.of(
+        new Membership()
+          .setType("USER")
+          .setPreferredMemberKey(new EntityKey().setId(member1.email)),
+        new Membership()
+          .setType("GROUP")
+          .setPreferredMemberKey(new EntityKey().setId(member2.email))));
+
+    var expander = new GroupExpander(groupsClient, new SynchronousExecutor());
+
+    var inputSet = Set.<PrincipalIdentifier>of(
+      TEST_USER_1,
+      TEST_USER_2,
+      TEST_GROUP_1,
+      TEST_GROUP_2);
+    var outputSet = expander.expand(inputSet);
+
+    assertEquals(4, outputSet.size());
+    assertTrue(outputSet.contains(TEST_USER_1));
+    assertTrue(outputSet.contains(TEST_USER_2));
+    assertTrue(outputSet.contains(member1));
+    assertTrue(outputSet.contains(member2));
+  }
+}

GoogleCloudPlatform/jit-groups

sources/src/main/java/com/google/solutions/jitaccess/core/auth/AccessControlList.java
@@ -21,23 +21,171 @@
 
 package com.google.solutions.jitaccess.core.auth;
 
+import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Allows access to a defined set of principals.
- * @param allowedPrincipals allowed principals
+ *
+ * @param entries list of access control list entries.
  */
 public record AccessControlList(
-  @NotNull Set<PrincipalId> allowedPrincipals
+  @NotNull Collection<Entry> entries
 ){
   /**
-   * Check whether a subject is allowed access.
+   * Check whether access is allowed access for any of the provided
+   * principals.
+   *
+   * Access control list entries are evaluated in order. Access is
+   * denied as soon as a matching deny-entry is encountered.
    */
-  public boolean isAllowed(@NotNull Subject subject) {
-    return this.allowedPrincipals
+  private static boolean isAllowed(
+    @NotNull Iterable<PrincipalId> principals,
+    Iterable<Entry> accessEntries,
+    int requiredAccessRights
+  ) {
+    Preconditions.checkArgument(requiredAccessRights != 0, "requiredAccessMask");
+
+    //
+    // NB. It's possible that no single principal is granted all the requested
+    // access. Therefore, we can't check each principal individually, but
+    // instead have to calculate the effective access mask across all principals.
+    //
+
+    int effectiveAccessMaskForSubject = 0;
+
+    for (var principal : principals) {
+      for (var entry : accessEntries) {
+        if (entry.principal.equals(principal)) {
+          if (entry instanceof AllowedEntry allowedEntry) {
+            //
+            // Accumulate access granted by this entry.
+            //
+            effectiveAccessMaskForSubject |= allowedEntry.accessRights;
+          }
+          else if (entry instanceof DeniedEntry deniedEntry &&
+            (deniedEntry.accessRights & requiredAccessRights) != 0) {
+            //
+            // At least one of the required access bits is denied.
+            //
+            return false;
+          }
+        }
+      }
+    }
+
+    return (effectiveAccessMaskForSubject & requiredAccessRights) == requiredAccessRights;
+  }
+
+  /**
+   * @return principals that have granted the requested level of access.
+   */
+  public Set<PrincipalId> allowedPrincipals(int requiredAccessRights) {
+    //
+    // Segment the list of entries by principal.
+    //
+    var entriesByPrincipals = this.entries
       .stream()
-      .anyMatch(id -> subject.principals().contains(id));
+      .collect(Collectors.groupingBy(e -> e.principal));
+
+    //
+    // Filter down the list of principals to those that have been
+    // granted the requested level of access.
+    //
+    return entriesByPrincipals
+      .entrySet()
+      .stream()
+      .filter(e -> isAllowed(
+        Collections.singleton(e.getKey()), // Principal
+        e.getValue(), // List of access control list entries
+        requiredAccessRights))
+      .map(e -> e.getKey())
+      .collect(Collectors.toSet());
+  }
+
+  /**
+   * Check whether a subject is allowed access.
+   *
+   * Access control list entries are evaluated in order. Access is
+   * denied as soon as a matching deny-entry is encountered.
+   */
+  public boolean isAllowed(@NotNull Subject subject, int requiredAccessRights) {
+    Preconditions.checkArgument(requiredAccessRights != 0, "requiredAccessRights");
+
+    return isAllowed(subject.principals(), this.entries, requiredAccessRights);
+  }
+
+  //---------------------------------------------------------------------------
+  // Inner classes.
+  //---------------------------------------------------------------------------
+
+  /**
+   * An access control list entry.
+   */
+  public static abstract class Entry {
+    /**
+     * Kind of access granted, where each bit represents a
+     * particular kind of access.
+     */
+    public final int accessRights;
+
+    /**
+     * Principal that is being granted access.
+     */
+    public final @NotNull PrincipalId principal;
+
+    protected Entry(@NotNull PrincipalId principal, int accessRights) {
+      this.principal = principal;
+      this.accessRights = accessRights;
+    }
+  }
+
+  /**
+   * Entry that grants certain access.
+   */
+  public static class AllowedEntry extends Entry {
+    public AllowedEntry(@NotNull PrincipalId principal, int accessMask) {
+      super(principal, accessMask);
+    }
+
+    @Override
+    public String toString() {
+      return String.format("Allow %s: %08X", this.principal, this.accessRights);
+    }
+  }
+
+  /**
+   * Entry that denies certain access.
+   */
+  public static class DeniedEntry extends Entry {
+    public DeniedEntry(@NotNull PrincipalId principal, int accessMask) {
+      super(principal, accessMask);
+    }
+
+    @Override
+    public String toString() {
+      return String.format("Deny %s: %08X", this.principal, this.accessRights);
+    }
+  }
+
+  public static class Builder {
+    private final List<Entry> entries = new LinkedList<>();
+
+    public Builder allow(PrincipalId principal, int accessMask) {
+      this.entries.add(new AllowedEntry(principal, accessMask));
+      return this;
+    }
+
+    public Builder deny(PrincipalId principal, int accessMask) {
+      this.entries.add(new DeniedEntry(principal, accessMask));
+      return this;
+    }
+
+    public AccessControlList build() {
+      return new AccessControlList(Collections.unmodifiableList(this.entries));
+    }
   }
 }

sources/src/main/java/com/google/solutions/jitaccess/core/catalog/jitrole/JitRoleActivator.java
@@ -1,0 +1,140 @@
+//
+// Copyright 2024 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+package com.google.solutions.jitaccess.core.catalog.jitrole;
+
+import com.google.common.base.Preconditions;
+import com.google.solutions.jitaccess.core.*;
+import com.google.solutions.jitaccess.core.auth.UserId;
+import com.google.solutions.jitaccess.core.catalog.*;
+import com.google.solutions.jitaccess.core.clients.CloudIdentityGroupsClient;
+import org.checkerframework.checker.units.qual.N;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.InstantSource;
+
+/**
+ * Activates JIT roles by modifying memberships to the Google group that backs
+ * the JIT role.
+ */
+public class JitRoleActivator extends EntitlementActivator<JitRole, OrganizationId, UserContext> {
+  private final @NotNull CloudIdentityGroupsClient groupsClient;
+  private final @NotNull JitRoleGroupMapping mapping;
+  private final @NotNull InstantSource clock;
+
+  public JitRoleActivator(
+    @NotNull Catalog<JitRole, OrganizationId, UserContext> catalog,
+    @NotNull CloudIdentityGroupsClient groupsClient,
+    @NotNull JitRoleGroupMapping mapping,
+    @NotNull JustificationPolicy policy,
+    @NotNull InstantSource clock
+  ) {
+    super(catalog, policy);
+
+    Preconditions.checkNotNull(groupsClient, "groupsClient");
+
+    this.groupsClient = groupsClient;
+    this.mapping = mapping;
+    this.clock = clock;
+  }
+
+  private Activation provisionGroupMembership(
+    @NotNull JitRole role,
+    @NotNull UserId user,
+    @NotNull Instant startTime,
+    @NotNull Duration duration
+  ) throws AccessException, IOException {
+
+    var group = this.mapping.groupFromJitRole(role);
+
+    //
+    // Add time-bound group membership. If the user is a member
+    // already, update the expiry of their membership.
+    //
+    // NB. Unlike with IAM conditions, we can't specify a start time
+    // for a temporary group memberships.
+    //
+    Preconditions.checkArgument(
+      !startTime.isAfter(this.clock.instant()),
+      "Start time must not be in the future");
+
+    this.groupsClient.addMembership(group, user, startTime.plus(duration));
+
+    return new Activation(startTime, duration);
+  }
+
+  //---------------------------------------------------------------------------
+  // Overrides.
+  //---------------------------------------------------------------------------
+
+  @Override
+  public int maximumNumberOfEntitlementsPerJitRequest() {
+    return 1;
+  }
+
+  @Override
+  protected Activation provisionAccess(
+    JitActivationRequest<JitRole> request
+  ) throws AccessException, AlreadyExistsException, IOException {
+    Preconditions.checkNotNull(request, "request");
+    Preconditions.checkArgument(
+      request.entitlements().size() == 1,
+      "Only one JIT role can be activated at a time");
+
+    return provisionGroupMembership(
+      request.entitlements().stream().findFirst().get(),
+      request.requestingUser(),
+      request.startTime(),
+      request.duration());
+  }
+
+  @Override
+  protected Activation provisionAccess(
+    UserId approvingUser,
+    MpaActivationRequest<JitRole> request
+  ) throws AccessException, AlreadyExistsException, IOException {
+    Preconditions.checkNotNull(request, "request");
+    Preconditions.checkArgument(
+      request.entitlements().size() == 1,
+      "Only one JIT role can be activated at a time");
+
+    //
+    // NB. The start/end time for the membership is derived from the approval token. If multiple
+    // reviewers try to approve the same token, the resulting condition (and binding) will
+    // be the same.
+    //
+
+    return provisionGroupMembership(
+      request.entitlements().stream().findFirst().get(),
+      request.requestingUser(),
+      request.startTime(),
+      request.duration());
+  }
+
+  @Override
+  public @NotNull JsonWebTokenConverter<MpaActivationRequest<JitRole>> createTokenConverter() {
+    //TODO: add membership!
+    throw new RuntimeException("NIY!");
+  }
+}

sources/src/test/java/com/google/solutions/jitaccess/core/auth/TestGroupExpander.java
@@ -1,0 +1,110 @@
+//
+// Copyright 2024 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+package com.google.solutions.jitaccess.core.auth;
+
+import com.google.api.services.cloudidentity.v1.model.EntityKey;
+import com.google.api.services.cloudidentity.v1.model.Membership;
+import com.google.solutions.jitaccess.core.clients.CloudIdentityGroupsClient;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Executor;
+
+public class TestGroupExpander {
+  private static final UserId TEST_USER_1 = new UserId("test-1@example.com");
+  private static final UserId TEST_USER_2 = new UserId("test-2@example.com");
+  private static final GroupId TEST_GROUP_1 = new GroupId("group-1@example.com");
+  private static final GroupId TEST_GROUP_2 = new GroupId("group-2@example.com");
+
+  private class SynchronousExecutor implements Executor {
+
+    @Override
+    public void execute(Runnable command) {
+      command.run();
+    }
+  }
+
+  @Test
+  public void whenSetEmpty_ThenExpandReturnsEquivalentSet() throws Exception {
+    var groupsClient = mock(CloudIdentityGroupsClient.class);
+    var expander = new GroupExpander(groupsClient, new SynchronousExecutor());
+
+    var inputSet = Set.<PrincipalId>of();
+    var outputSet = expander.expand(inputSet);
+    assertEquals(0, outputSet.size());
+  }
+
+  @Test
+  public void whenSetContainsNoGroups_ThenExpandReturnsEquivalentSet() throws Exception {
+    var groupsClient = mock(CloudIdentityGroupsClient.class);
+    var expander = new GroupExpander(groupsClient, new SynchronousExecutor());
+
+    var inputSet = Set.<PrincipalId>of(
+      TEST_USER_1,
+      TEST_USER_2);
+    var outputSet = expander.expand(inputSet);
+
+    assertEquals(2, outputSet.size());
+    assertTrue(outputSet.contains(TEST_USER_1));
+    assertTrue(outputSet.contains(TEST_USER_2));
+  }
+
+  @Test
+  public void whenSetContainsGroups_ThenExpandReturnsExpandedSet() throws Exception {
+    var member1 = new UserId("member-1@example.com");
+    var member2 = new GroupId("member-1@example.com");
+
+    var groupsClient = mock(CloudIdentityGroupsClient.class);
+    when(groupsClient.listMemberships(TEST_GROUP_1))
+      .thenReturn(List.of(new Membership()
+        .setType("SERVICE_ACCOUNT")
+        .setPreferredMemberKey(new EntityKey().setId("sa@example.com"))));
+    when(groupsClient.listMemberships(TEST_GROUP_2))
+      .thenReturn(List.of(
+        new Membership()
+          .setType("USER")
+          .setPreferredMemberKey(new EntityKey().setId(member1.email)),
+        new Membership()
+          .setType("GROUP")
+          .setPreferredMemberKey(new EntityKey().setId(member2.email))));
+
+    var expander = new GroupExpander(groupsClient, new SynchronousExecutor());
+
+    var inputSet = Set.<PrincipalId>of(
+      TEST_USER_1,
+      TEST_USER_2,
+      TEST_GROUP_1,
+      TEST_GROUP_2);
+    var outputSet = expander.expand(inputSet);
+
+    assertEquals(4, outputSet.size());
+    assertTrue(outputSet.contains(TEST_USER_1));
+    assertTrue(outputSet.contains(TEST_USER_2));
+    assertTrue(outputSet.contains(member1));
+    assertTrue(outputSet.contains(member2));
+  }
+}