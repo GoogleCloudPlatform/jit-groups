c21fb6b1148712c684bbae1ecfdc3fb9207ebbe8
Johannes Passing <jpassing@hotmail.com>
2024-08-13 15:40:12 +1000

    Infer environment name of oobe policy

--- sources/src/main/java/com/google/solutions/jitaccess/web/EnvironmentConfiguration.java
+++ sources/src/main/java/com/google/solutions/jitaccess/web/EnvironmentConfiguration.java
@@ -24,6 +24,7 @@ package com.google.solutions.jitaccess.web;
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ImpersonatedCredentials;
+import com.google.common.io.Files;
 import com.google.common.util.concurrent.UncheckedExecutionException;
 import com.google.solutions.jitaccess.apis.clients.HttpTransport;
 import com.google.solutions.jitaccess.apis.clients.ResourceManagerClient;
@@ -154,6 +155,16 @@ class EnvironmentConfiguration implements PolicyHeader {
       }
     }
 
+    //
+    // Set default environment name in metadata so that
+    // the YAML itself doesn't need to specify a name.
+    //
+    var metadata = new Policy.Metadata(
+      "built-in policy",
+      Instant.now(),
+      null,
+      environmentName);
+
     return new EnvironmentConfiguration(
       environmentName,
       description,
@@ -168,9 +179,7 @@ class EnvironmentConfiguration implements PolicyHeader {
 
           var policy = new String(stream.readAllBytes());
           return PolicyDocument
-            .fromString(
-              policy,
-              new Policy.Metadata("built-in policy", Instant.now()))
+            .fromString(policy, metadata)
             .policy();
         }
         catch (Exception e) {
